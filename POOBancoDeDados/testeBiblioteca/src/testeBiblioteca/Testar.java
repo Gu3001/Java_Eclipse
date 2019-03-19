5e6e73de9c88791722d7afa702ad127c87bc4d58
Luiz Gustavo <37215681+Gu3001@users.noreply.github.com>
2019-03-18 22:36:41 -0300

    Iniciando projeto top

--- POOBancoDeDados/testeBiblioteca/src/testeBiblioteca/Testar.java
+++ POOBancoDeDados/testeBiblioteca/src/testeBiblioteca/Testar.java
@@ -2,6 +2,7 @@ package testeBiblioteca;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
 
@@ -19,23 +20,33 @@ public class Testar {
 		Connection conn = DriverManager.getConnection(url, user,"");
 		Statement st = conn.createStatement();
 		
-
 		
 		String resposta = "s";
 		while(resposta.equals("s")){
 			resposta = JOptionPane.showInputDialog("Insira dua resposta S ou N");
-			
+	
 			if (resposta.equals("n")){
 				break;
 			}
-			
-			String nome = JOptionPane.showInputDialog("Insira o Nome do Estado");
+			String nome = JOptionPane.showInputDialog("Insira o Nome");
+			String cpf = JOptionPane.showInputDialog("Insira o seu cpf");
+			String nomeEstado = JOptionPane.showInputDialog("Insira o seu Estado");
 			String sigla = JOptionPane.showInputDialog("Insira a sigla do Estado");
-			st.executeUpdate("INSERT INTO ESTADO(NOME,SIGLA) VALUES('"+nome+"','"+sigla+"')");
+			
+			
+			
+			st.executeUpdate("INSERT INTO PESSOA(NOME,CPF) VALUES('"+nome+"','"+cpf+"')");
+			st.executeUpdate("INSERT INTO ESTADO(NOMEESTADO,SIGLA) VALUES('"+nomeEstado+"','"+sigla+"')");
+
 			
 
 		}
-		
+		ResultSet rs = st.executeQuery("SELECT * FROM PESSOA");
+		String resultado = "";
+		while(rs.next()) {
+			resultado += rs.getString("nome")+"\n";
+		}
+		JOptionPane.showMessageDialog(null, resultado);
 		
 		
 		}catch(Exception e) {
